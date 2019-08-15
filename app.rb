8ff11f9b2a3008f5ec579fdf39a986054d5d2b65
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-15 19:50:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ end
 
 get '/say/:number/:phrase' do
   @number = params[:number].to_i
-  @phrase =  params[:phrase] end
-  "#{@phrase}"
+  @phrase =  params[:phrase] 
+  "#{@number.times do @phrase}"
 end
 
 get "/say/:word1/:word2/:word3/:word4/:word5" do
