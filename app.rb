40d8c43dfa9df04c137050577f9404f47eebaed8
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-15 20:16:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,8 +24,8 @@ end
 
  get "/:operation/:number1/:number2"  do
   @operation = params[:operation]
-   @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i
+  @number1 = params[:number1].to_i
+  @number2 = params[:number2].to_i
     
     case @operation 
     when "add"
