8d55c7a852f37663545e20cbf45faa336ad30be4
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-15 20:19:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,11 +29,11 @@ end
     
     case @operation 
     when "add"
-      @number1 + @number2
+      "#{@number1 + @number2}"
     when "subtract"
-      @number1 - @number2
+     "#{ @number1 - @number2}"
     when "multiply"
-      @number1 * @number2
+      "#{@number1 * @number2}"
     when "divide"
       "#{@number1 / @number2}"
     end
